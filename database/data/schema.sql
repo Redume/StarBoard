77825c37a93ddb1fb4665f7f0b7611ee01012945
Redume <danilivasenko09@gmail.com>
2024-12-05 21:20:22 +0300

    chore: Added sql schemas

---
+++ database/data/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE message
+(
+    message_id BIGINT NOT NULL,
+    chat_id BIGINT NOT NULL,
+    reaction_count INT DEFAULT 0
+);
+
+CREATE TABLE chat
+(
+    chat_id            BIGINT NOT NULL,
+    channel_id         BIGINT,
+    min_reaction_count INT DEFAULT 5,
+    emoji_list         TEXT[] DEFAULT array['⭐']
+);
